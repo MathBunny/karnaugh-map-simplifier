2e9e2286cd81ff942cc628cefb8e1f4b76489c0e
MathBunny <horatiulazu@gmail.com>
2016-09-02 23:02:02 -0400

    Add coordinate docs

--- src/main/java/Coordinate.java
+++ src/main/java/Coordinate.java
@@ -5,25 +5,47 @@
  * Created by horatiulazu on 2016-08-30.
  */
 public class Coordinate {
+    /** x int This is the x value */
     private int x;
+    /** y int This is the y coordinate */
     private int y;
+    /** reflectedBinaryCode String This is the reflected binary code for the coordinate */
     private String reflectedBinaryCode;
 
+    /**
+     * Class constructor for standard coordinate.
+     * @param x int This is the x value
+     * @param y int This is the y value
+     */
     public Coordinate(int x, int y){
         this.x = x;
         this.y = y;
     }
 
+    /**
+     * This is the class constructor for reflected binary code values inside Karnaugh maps
+     * @param x int This is the x value
+     * @param y int This is the y value
+     * @param reflectedBinaryCode String This is the reflected binary code
+     */
     public Coordinate(int x, int y, String reflectedBinaryCode){
         this.x = x;
         this.y = y;
         this.reflectedBinaryCode = reflectedBinaryCode;
     }
 
+    /**
+     * Returns x coordinate value
+     * @return int This is the x value
+     */
     public int getX(){
         return x;
     }
 
+    /**
+     * Returns y coordinate value
+     * @return int This is the y value
+     */
     public int getY(){
         return y;
     }
