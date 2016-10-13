eb158178e661c1870faf6337bab865ca85c824a3
MathBunny <horatiulazu@gmail.com>
2016-10-13 11:40:38 -0400

    Uncomment tests

--- src/main/java/Settings.java
+++ src/main/java/Settings.java
@@ -10,6 +10,10 @@ public class Settings {
     public static String AND_CHARACTER = ""; //∧
     /** OR_CHARACTER String This contains the or character */
     public static String OR_CHARACTER = "+"; //∨
+    /** NOT_CHARACTER String This is the not character */
+    public static String NOT_CHARACTER = "*"; //¬
+    /** IS_DEFAULT_NOTATION boolean Indicates if the symbols are in algebraic notation or not */
+    public static boolean IS_DEFAULT_NOTATION = false;
 
     /** This method returns real-time setting
      * @return boolean This indicates if it is real-time
@@ -25,6 +29,11 @@ public class Settings {
         REALTIME_GRAPHICS = !REALTIME_GRAPHICS;
     }
 
+    /** This method toggles the notation */
+    public void toggleNotation(){
+        IS_DEFAULT_NOTATION = !IS_DEFAULT_NOTATION;
+    }
+
     /**
      * This method toggles characters from algebraic and circuit style notation
      */
@@ -32,10 +41,12 @@ public class Settings {
         if (AND_CHARACTER.equals("")){
             AND_CHARACTER = "∧";
             OR_CHARACTER = "∨";
+            NOT_CHARACTER = "¬";
         }
         else{
             AND_CHARACTER = "";
             OR_CHARACTER = "+";
+            NOT_CHARACTER = "'";
         }
     }
 }
--- src/test/java/PrefixSumTest.java
+++ src/test/java/PrefixSumTest.java
@@ -1,12 +1,12 @@
-//import org.junit.Test;
-//import static junit.framework.TestCase.assertEquals;
+import org.junit.Test;
+import static junit.framework.TestCase.assertEquals;
 
 /**
  * This class tests prefix sum arrays.
  * @author Horatiu Lazu
  */
 public class PrefixSumTest {
-    /*
+
     @Test
     public void testSumRegion (String [] args) {
         int[][] multi = new int[][]{
@@ -18,5 +18,5 @@ public class PrefixSumTest {
         };
 
         assertEquals("Sum from (0, 0) to (2, 2) should be 2", new PrefixSum(multi).sumRegion(0, 0, 2, 2), 2); //2
-    }*/
+    }
 }
