0c1cce1a30ce2bacb2deeb4f77cb558d8f82059b
MathBunny <horatiulazu@gmail.com>
2016-12-16 10:34:30 -0500

    Add JUnit 3 prefix sum test

--- src/test/java/PrefixSumTest.java
+++ src/test/java/PrefixSumTest.java
@@ -1,14 +1,18 @@
-//import org.junit.Test;
-//import static junit.framework.TestCase.assertEquals;
+import junit.framework.TestCase;
 
 /**
- * This class tests prefix sum arrays.
- * @author Horatiu Lazu
+ * Created by horatiulazu on 2016-12-16.
  */
-public class PrefixSumTest {
-    /*
-    @Test
-    public void testSumRegion (String [] args) {
+public class PrefixSumTest extends TestCase {
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void tearDown() throws Exception {
+
+    }
+
+    public void testSumRegion() throws Exception {
         int[][] multi = new int[][]{
                 {0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
                 {0, 0, 1, 0, 0, 0, 0, 0, 0, 0},
@@ -16,7 +20,7 @@ public class PrefixSumTest {
                 {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
                 {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
         };
+        assertEquals("Sum from (0, 0) to (2, 2) should be 3", new PrefixSum(multi).sumRegion(0, 0, 2, 2), 3);
+    }
 
-        assertEquals("Sum from (0, 0) to (2, 2) should be 2", new PrefixSum(multi).sumRegion(0, 0, 2, 2), 2); //2
-    }*/
 }
