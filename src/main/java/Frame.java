a41efdad196d94760c37b378113bfee415a38e74
MathBunny <horatiulazu@gmail.com>
2017-06-17 16:31:12 -0400

    Add keyboard shortcuts to menuitems

--- src/main/java/Frame.java
+++ src/main/java/Frame.java
@@ -21,6 +21,7 @@ public class Frame extends Application {
         Scene scene = new Scene(root, 960, 640);
 
         stage.setTitle("Karnaugh Map Simplification Software");
+        stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
     }
